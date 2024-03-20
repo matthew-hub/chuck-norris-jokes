67b7d72b012607c1aa4ef76babc4e78c3684f95c
matthew-hub <mat.ressel97@gmail.com>
2024-03-20 16:52:00 +0100

    useLocation: change the link name depending on current path

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,16 +1,31 @@
+import { Link, useLocation } from "react-router-dom";
 import "./Topbar.css";
 
-const Topbar = () => {
+function Topbar() {
+  const { pathname } = useLocation();
+
+  const liContent = () => {
+    if (pathname !== "/about-chuck-norris") {
+      return <Link to="/about-chuck-norris">About Chuck</Link>;
+    }
+    return <Link to="/">Jokes</Link>;
+  };
+
   return (
     <>
       <div className="app-topbar">
-        <span>New Joke</span>
-        <span>About Chuck</span>
-
-        <span>EN | PL</span>
+        <nav>
+          <ul>
+            <li>{liContent()}</li>
+            <li>
+              <Link to="/new-joke">New Joke</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* <span>EN | PL</span> */}
       </div>
     </>
   );
-};
+}
 
 export default Topbar;
