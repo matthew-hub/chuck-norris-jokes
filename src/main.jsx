f8d779588b30fb43cfe0477d0de0151d0bad976a
matthew-hub <mat.ressel97@gmail.com>
2024-03-20 16:47:26 +0100

    Add: React Router, Create: Routes home, about

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.jsx";
+import Home from "./views/Home/Home.jsx";
+import AboutChuck from "./views/AboutChuck/AboutChuck.jsx";
+import ErrorPage from "./error-page";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about-chuck-norris",
+        element: <AboutChuck />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header.jsx";
+
+import Joke from "../../components/Joke/Joke.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Header title="Chuck Norris Jokes" />
+      <Joke />
+    </>
+  );
+};
+
+export default Home;
