2a7dd50fa0ad7ede77653a7f2e21f2fcc1a6c92a
matthew-hub <mat.ressel97@gmail.com>
2024-03-30 20:57:41 +0100

    change: createBrowserRouter to createRouterFromElements

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,22 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./views/Home/Home.jsx";
 import AboutChuck from "./views/AboutChuck/AboutChuck.jsx";
 import ErrorPage from "./error-page";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about-chuck-norris",
-        element: <AboutChuck />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" id="root" element={<App />} errorElement={<ErrorPage />}>
+      <Route path="" element={<Home />} />
+      <Route path="about-chuck-norris" element={<AboutChuck />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>
 );
